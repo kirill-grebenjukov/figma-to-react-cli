aa68e7291be38fb1a66e69e36f3ddf8f3adba1d4
kirill_grebenjukov <grebenukov.kirill@gmail.com>
2020-01-29 02:08:34 +0300

    Fixed error in stories; Added configuration for stories section;

--- src/file-generator/storybook.js
+++ src/file-generator/storybook.js
@@ -11,19 +11,25 @@ export default function exportJSFile(
   const {
     exportCode: { path: exportCodePath },
     exportSvgComponents: { path: exportSvgPath },
+    storybook: { codeSection = 'Components', svgSection = 'SVG' },
   } = config;
 
   const { componentName, componentPath, svgCode } = component;
 
   const exportPath = svgCode ? exportSvgPath : exportCodePath;
+  const storiesSection = svgCode ? svgSection : codeSection;
+  const componentNameKebab = kebabCase(componentName);
 
-  let jsCode = template.split('{{componentName}}').join(componentName);
+  let jsCode = template
+    .replace('{{componentPath}}', componentNameKebab)
+    .replace('{{storiesOf}}', storiesSection)
+    .split('{{componentName}}')
+    .join(componentName);
 
   if (prettierOptions) {
     jsCode = prettier.format(jsCode, prettierOptions);
   }
 
-  const componentNameKebab = kebabCase(componentName);
   const fileDir = path.join(exportPath, componentPath, componentNameKebab);
   fs.mkdirSync(fileDir, { recursive: true });
 
