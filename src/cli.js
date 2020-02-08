762b340c8f51ff0991349b5d078b19a19d2859f0
kirill_grebenjukov <grebenukov.kirill@gmail.com>
2020-02-08 15:50:40 +0300

    Moved cli to cli.js src/index.js now has fields that are helpful figma-to-react-cli.config.js in a consumer's app

---
+++ src/cli.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+import Promise from 'bluebird';
+import * as Figma from 'figma-api';
+import _ from 'lodash';
+
+import { cosmiconfigSync } from 'cosmiconfig';
+
+import parseFigma from './parser';
+import exportFiles from './file-generator';
+import { findCanvas, findNodeByName } from './utils';
+import { STORE_NAME, TEXT_STORE_NAME } from './constants';
+import defaultConfig from './default-config';
+
+const { config: cfg } = cosmiconfigSync('figma-to-react-cli').search();
+const config = _.defaultsDeep(cfg, defaultConfig);
+
+const {
+  figma: { personalAccessToken, fileKey, pageName },
+} = config;
+
+// https://www.figma.com/developers/api
+const figmaApi = new Figma.Api({ personalAccessToken });
+
+Promise.all([figmaApi.getFile(fileKey), figmaApi.getImageFills(fileKey)])
+  .then(
+    async ([
+      { document },
+      {
+        meta: { images: imagesJson },
+      },
+    ]) => {
+      console.log('Getting data from Figma...');
+      const pageJson = pageName ? findCanvas(document, pageName) : document;
+      if (!pageJson) {
+        console.log(`Can not find page/canvas with name '${pageName}'`);
+        return;
+      }
+
+      const settingsFrame = findNodeByName(pageJson, STORE_NAME);
+      if (!settingsFrame) {
+        console.log(`Can not find Frame with name '${STORE_NAME}'`);
+        return;
+      }
+
+      const settingsTextNode = findNodeByName(settingsFrame, TEXT_STORE_NAME);
+      if (!settingsTextNode) {
+        console.log(`Can not find TextNode with name '${TEXT_STORE_NAME}'`);
+        return;
+      }
+
+      const settingsText = settingsTextNode.characters;
+      if (!settingsText) {
+        console.log('TextNode with settings is empty. No reason to proceed.');
+        return;
+      }
+
+      const settingsJson = JSON.parse(settingsText);
+
+      console.log('Parsing...');
+      const context = { ...config, figmaApi };
+      const sourceMap = await parseFigma({
+        pageJson,
+        imagesJson,
+        settingsJson,
+        context,
+      });
+
+      console.log('Exporting...');
+      await exportFiles({ sourceMap, context });
+
+      console.log('All done!');
+    },
+  )
+  .catch(console.error);
